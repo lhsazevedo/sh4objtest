762aba793567b4b51775480fd81c659b08ae014a
Lucas Azevedo <lhs_azevedo@hotmail.com>
2026-06-14 18:53:45 -0300

    Use backed int enum for ChunkType

--- src/Parser/Chunk.php
+++ src/Parser/Chunk.php
@@ -3,25 +3,12 @@
 namespace Lhsazevedo\Sh4ObjTest\Parser;
 
 class Chunk {
-    public ChunkType $type;
+    public ?ChunkType $type;
 
     public function __construct(
         int $type,
         public string $data,
     ) {
-        $this->type = match ($type & 0x7f) {
-            0x04 => ChunkType::ModuleHeader,
-            0x06 => ChunkType::UnitHeader,
-            0x07 => ChunkType::UnitDebug,
-            0x08 => ChunkType::SectionHeader,
-            0x0c => ChunkType::Imports,
-            0x14 => ChunkType::Exports,
-            0x1a => ChunkType::SectionSelection,
-            0x1c => ChunkType::ObjectData,
-            0x20 => ChunkType::Relocation,
-            0x38 => ChunkType::DebugLines,
-            0x7f => ChunkType::Termination,
-            default => ChunkType::Unknown,
-        };
+        $this->type = ChunkType::tryFrom($type & 0x7f);
     }
 }
--- src/Parser/ChunkType.php
+++ src/Parser/ChunkType.php
@@ -4,17 +4,16 @@ declare(strict_types=1);
 
 namespace Lhsazevedo\Sh4ObjTest\Parser;
 
-enum ChunkType {
-    case ModuleHeader;
-    case UnitHeader;
-    case UnitDebug;
-    case SectionHeader;
-    case Imports;
-    case Exports;
-    case SectionSelection;
-    case ObjectData;
-    case Relocation;
-    case DebugLines;
-    case Termination;
-    case Unknown;
+enum ChunkType: int {
+    case ModuleHeader    = 0x04;
+    case UnitHeader      = 0x06;
+    case UnitDebug       = 0x07;
+    case SectionHeader   = 0x08;
+    case Imports         = 0x0c;
+    case Exports         = 0x14;
+    case SectionSelection = 0x1a;
+    case ObjectData      = 0x1c;
+    case Relocation      = 0x20;
+    case DebugLines      = 0x38;
+    case Termination     = 0x7f;
 }
